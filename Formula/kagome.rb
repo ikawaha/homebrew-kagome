e0202dd091bbf06f8b87415515c0ebf80e3b91e3
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-29 22:58:45 +0900

    Brew formula update for kagome version v2.4.1

--- Formula/kagome.rb
+++ Formula/kagome.rb
@@ -2,24 +2,24 @@
 class Kagome < Formula
   desc "Self-contained Japanese Morphological Analyzer written in pure Go."
   homepage "https://github.com/ikawaha/kagome"
-  version "2.4.0"
+  version "2.4.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ikawaha/kagome/releases/download/v2.4.0/kagome_2.4.0_darwin_amd64.tar.gz"
-    sha256 "9e395e54b10220e250de5fb424cbb52d299c261de60db81084c2a1e82929e659"
+    url "https://github.com/ikawaha/kagome/releases/download/v2.4.1/kagome_2.4.1_darwin_amd64.tar.gz"
+    sha256 "b2a35773e0b178e85c53de4a59ce5d03ec37af62252be470da9bc0523793251c"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ikawaha/kagome/releases/download/v2.4.0/kagome_2.4.0_linux_amd64.tar.gz"
-    sha256 "3140b6e19a105e5f05b190b87ac7d4739865cbe6869bc4cf091a83e12948f2be"
+    url "https://github.com/ikawaha/kagome/releases/download/v2.4.1/kagome_2.4.1_linux_amd64.tar.gz"
+    sha256 "f727a7854570f47c6ec723ae3da2d09979ae859c2b138ca2f471fdf1b08e5a55"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/ikawaha/kagome/releases/download/v2.4.0/kagome_2.4.0_linux_armv6.tar.gz"
-    sha256 "71a49e76d83ed72086a77c903f3ab61c62d46020741b6b38e51e59dac7cbf21f"
+    url "https://github.com/ikawaha/kagome/releases/download/v2.4.1/kagome_2.4.1_linux_armv6.tar.gz"
+    sha256 "27c215ecc6c79cd4e40b3870a4f2a0ca2639985de4f9d2b877b782eb2ea99a20"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/ikawaha/kagome/releases/download/v2.4.0/kagome_2.4.0_linux_arm64.tar.gz"
-    sha256 "4523f4f09e2af62f2db429f3c4fd68121142f25bbf4af4917fed573f3c5f2b17"
+    url "https://github.com/ikawaha/kagome/releases/download/v2.4.1/kagome_2.4.1_linux_arm64.tar.gz"
+    sha256 "ed674c5f2263be47a371c26b5bba4bca304fd2cc093aaf182e3e3d6fd3a2d7f4"
   end
 
   def install
